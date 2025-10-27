2aa7c1e7078d0c0b5d6d8b6867669a3ad54c720f
Ankit Kumar <contactmrtechboy@gmail.com>
2025-10-27 12:50:33 +0530

    autoplay

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -23,7 +23,7 @@ const MovieDetails = () => {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/b9EkMc79ZSU"
+            src="https://www.youtube.com/embed/b9EkMc79ZSU?autoplay=1&mute=1"
             title="Stranger Things Trailer"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
